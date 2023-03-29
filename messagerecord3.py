277596aff6551661f2f2852a58032f4dd6d7fb32
kostotpe <a19910304@hotmail.com>
2023-03-29 16:48:18 +0800

    have_space

---
+++ messagerecord3.py
@@ -0,0 +1,13 @@
+# 如果沒空格怎麼提取資料
+
+lines = []
+with open('3.txt', 'r', encoding='utf-8-sig') as f:
+    for line in f:
+        lines.append(line.strip())
+
+for line in lines:
+    s = line.split(' ')
+    time = s[0][:5]
+    name = s[0][5:]
+    print(name)
+
